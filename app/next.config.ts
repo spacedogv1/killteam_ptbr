8f841c1785cc02e188a0415d5966ff6350e77724
SpaceDog <destroyergamerrss@gmail.com>
2026-08-24 21:45:54 -0300

    Update next.config.ts

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const githubPagesBasePath =
+  process.env.GITHUB_PAGES === 'true' ? '/killteam_ptbr' : '';
+
+const nextConfig: NextConfig = {
+  assetPrefix: githubPagesBasePath || undefined,
+};
 
 export default nextConfig;
